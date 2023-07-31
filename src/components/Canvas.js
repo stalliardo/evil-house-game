d30fe99bc3d9e4015406927a1a58a5f2039c0bc8
stalliardo187 <darrenstallard27@hotmail.com>
2023-07-31 15:34:00 +0100

    cancelled the animation if the modal is in view

--- src/components/Canvas.js
+++ src/components/Canvas.js
@@ -11,12 +11,54 @@ import { updateLevelData } from "@/features/gameState/gameStateSlice";
 
 
 const Canvas = ({ ...props }) => {
-    const canvas = useRef();
+    const canvas = useRef(null);
+
+
     const [ctx, setCtx] = useState(null);
     const dispatch = useDispatch();
     const levelData = useSelector((state) => state.gameState);
-    // const isModalOpen = useSelector((state) => state.modal.isOpen);
-    // const isModalOpen = useSelector((state) => state.modal.isOpen);
+    const playerRef = useRef(new SpriteAnimation('allCharacters.png', 7, 4, 1, 2, { x: 200, y: 100 }, { x: 0, y: 0 }, ctx)); // cant use until ctx is set
+
+
+
+    
+
+
+
+
+
+
+
+
+    const isModalOpen = useSelector((state) => state.modal.isOpen);
+    const [pauseAnimation, setPauseAnimation] = useState(false);
+
+    const keys = {
+        w: {
+            pressed: false
+        },
+        a: {
+            pressed: false
+        },
+        s: {
+            pressed: false
+        },
+        d: {
+            pressed: false
+        },
+        e: {
+            pressed: false
+        }
+    }
+
+    let lastKey = "";
+
+    let playerIsAtInteractableBoundary = false;
+    const spriteSheet = new Image();
+    spriteSheet.src = "dungeonTileset.png";
+
+    const player = new SpriteAnimation('allCharacters.png', 7, 4, 1, 2, { x: 200, y: 100 }, { x: 0, y: 0 }, ctx);
+
 
     useEffect(() => {
         if (canvas.current) {
@@ -27,35 +69,12 @@ const Canvas = ({ ...props }) => {
         }
     }, []);
 
-        
-    if (ctx !== null) {
-        const keys = {
-            w: {
-                pressed: false
-            },
-            a: {
-                pressed: false
-            },
-            s: {
-                pressed: false
-            },
-            d: {
-                pressed: false
-            },
-            e: {
-                pressed: false
-            }
-        }
+    useEffect(() => {
+        let animationFrameId;
 
-        let lastKey = "";
         const boundaries = [];
-        let playerIsAtInteractableBoundary = false;
-        const spriteSheet = new Image();
-        spriteSheet.src = "dungeonTileset.png";
-                
-        const player = new SpriteAnimation('allCharacters.png', 7, 4, 1, 2, { x: 200, y: 100 }, { x: 0, y: 0 }, ctx);   
-               
-        levelData.map.forEach((row, i) => {
+
+        levelData.map?.forEach((row, i) => {
             row.forEach((symbol, j) => {
                 if (dataLoader(levelData.level, symbol, i, j, ctx) !== undefined) {
                     boundaries.push(dataLoader(levelData.level, symbol, i, j, ctx));
@@ -63,33 +82,22 @@ const Canvas = ({ ...props }) => {
             })
         })
 
-        function rectanglesCollide({ rect1, rect2 }) {
-            return (
-                rect1.position.y + rect1.velocity.y <= rect2.position.y + rect2.height && // top
-                rect1.position.x + rect1.width + rect1.velocity.x >= rect2.position.x && // right
-                rect1.position.y + rect1.height + rect1.velocity.y >= rect2.position.y && // bottom
-                rect1.position.x + rect1.velocity.x <= rect2.position.x + rect2.width // left
-            )
-        }
+        if (ctx) {
+            
 
-        let interactedBoundary;
-        function isObjectCollision(boundary) {
-            if (boundary.isInteractable) {
-                playerIsAtInteractableBoundary = true;
-                interactedBoundary = boundary;
-            }
-        }
 
-        function resetBoundaryAndInteractableBool() {
-            playerIsAtInteractableBoundary = false;
-            interactedBoundary = null;
-        }
+            const animate = () => {
 
-        function animate() {
-            requestAnimationFrame(animate);
-            ctx.clearRect(0, 0, canvas.current.width, canvas.current.height)
+                animationFrameId = requestAnimationFrame(animate);
 
-            // if (!isModalOpen) {
+                if(isModalOpen){
+                    console.log("is modal open called");
+                    cancelAnimationFrame(animationFrameId)
+                }
+
+                ctx.clearRect(0, 0, canvas.current.width, canvas.current.height)
+
+                // if (!isModalOpen) {
                 if (keys.w.pressed && lastKey === "w") {
                     for (let i = 0; i < boundaries.length; i++) {
                         const boundary = boundaries[i];
@@ -161,41 +169,41 @@ const Canvas = ({ ...props }) => {
                         }
                     }
                 }
-            // }
+                // }
 
-            boundaries.forEach((boundary) => {
-                boundary.draw(spriteSheet)
+                boundaries.forEach((boundary) => {
+                    boundary.draw(spriteSheet)
 
-                if (boundary.hasCollision === true) {
-                    if (
-                        rectanglesCollide({
-                            rect1: player,
-                            rect2: boundary
-                        })
-                    ) {
-                        player.velocity.x = 0;
-                        player.velocity.y = 0;
+                    if (boundary.hasCollision === true) {
+                        if (
+                            rectanglesCollide({
+                                rect1: player,
+                                rect2: boundary
+                            })
+                        ) {
+                            player.velocity.x = 0;
+                            player.velocity.y = 0;
+                        }
+                    }
+                })
+
+                player.update();
+                // player.drawSprite(ctx, 100, 100);
+
+                if (playerIsAtInteractableBoundary) {
+                    const pressEPrompt = new Prompt("Press 'E'", (levelData.map[0].length * 32 / 2 - 40), (levelData.map.length * 32 / 2), canvas.current, ctx);
+                    pressEPrompt.draw()
+                } else {
+                    if (keys.e.pressed) {
+                        props.closeInteractionDisplay()
+                        keys.e.pressed = false;
                     }
                 }
-            })
 
-            player.update();
-            // player.drawSprite(ctx, 100, 100);
+                player.velocity.y = 0;
+                player.velocity.x = 0;
 
-            if (playerIsAtInteractableBoundary) {
-                const pressEPrompt = new Prompt("Press 'E'", (levelData.map[0].length * 32 / 2 - 40), (levelData.map.length * 32 / 2), canvas.current, ctx);
-                pressEPrompt.draw()
-            } else {
-                if (keys.e.pressed) {
-                    props.closeInteractionDisplay()
-                    keys.e.pressed = false;
-                }
-            }
-
-            player.velocity.y = 0;
-            player.velocity.x = 0;
-
-            // if (!isModalOpen) {
+                // if (!isModalOpen) {
                 if (keys.w.pressed && lastKey === "w") {
                     player.velocity.y = -5;
                 } else if (keys.a.pressed && lastKey === "a") {
@@ -207,68 +215,110 @@ const Canvas = ({ ...props }) => {
                     player.velocity.x = +5;
                     player.setDirection("right");
                 }
-            // }
-        }
-
-        animate();
-
-        addEventListener("keydown", ({ key }) => {
-            switch (key) {
-                case "w":
-                    keys.w.pressed = true;
-                    lastKey = "w";
-                    props.closeInventory();
-                    break;
-                case "a":
-                    keys.a.pressed = true;
-                    lastKey = "a";
-                    player.isFacingLeft = true;
-                    props.closeInventory();
-                    break;
-                case "s":
-                    keys.s.pressed = true;
-                    lastKey = "s";
-                    props.closeInventory();
-                    break;
-                case "d":
-                    keys.d.pressed = true;
-                    lastKey = "d";
-                    player.isFacingLeft = false;
-                    props.closeInventory();
-                    break;
-                case "e":
-                    if (playerIsAtInteractableBoundary) {
-                        keys.e.pressed = true;
-                        props.onInteraction(interactedBoundary);
-                    }
-                    break;
-                case "i":
-                    if (playerIsAtInteractableBoundary) {
-                        props.showInventory(interactedBoundary);
-                    } else {
-                        props.showInventory();
-                    }
-                    break;
+                // }
             }
-        })
 
-        addEventListener("keyup", ({ key }) => {
-            switch (key) {
-                case "w":
-                    keys.w.pressed = false;
-                    break;
-                case "a":
-                    keys.a.pressed = false;
-                    break;
-                case "s":
-                    keys.s.pressed = false;
-                    break;
-                case "d":
-                    keys.d.pressed = false;
-                    break;
-            }
-        })
+            ////////////////////////////
+
+            animate();
+        };
+        return () => {
+            cancelAnimationFrame(animationFrameId);
+        };
+    }, [levelData, ctx, isModalOpen]);
+
+
+
+
+    // if (isModalOpen) {
+    //     console.log("Modla is opne pause the animation");
+    //     setPauseAnimation(true);
+    // }
+
+
+    // if (ctx !== null) {
+
+
+    function rectanglesCollide({ rect1, rect2 }) {
+        return (
+            rect1.position.y + rect1.velocity.y <= rect2.position.y + rect2.height && // top
+            rect1.position.x + rect1.width + rect1.velocity.x >= rect2.position.x && // right
+            rect1.position.y + rect1.height + rect1.velocity.y >= rect2.position.y && // bottom
+            rect1.position.x + rect1.velocity.x <= rect2.position.x + rect2.width // left
+        )
     }
+
+    let interactedBoundary;
+    function isObjectCollision(boundary) {
+        if (boundary.isInteractable) {
+            playerIsAtInteractableBoundary = true;
+            interactedBoundary = boundary;
+        }
+    }
+
+    function resetBoundaryAndInteractableBool() {
+        playerIsAtInteractableBoundary = false;
+        interactedBoundary = null;
+    }
+
+
+    addEventListener("keydown", ({ key }) => {
+        switch (key) {
+            case "w":
+                keys.w.pressed = true;
+                lastKey = "w";
+                props.closeInventory();
+                break;
+            case "a":
+                keys.a.pressed = true;
+                lastKey = "a";
+                player.isFacingLeft = true;
+                props.closeInventory();
+                break;
+            case "s":
+                keys.s.pressed = true;
+                lastKey = "s";
+                props.closeInventory();
+                break;
+            case "d":
+                keys.d.pressed = true;
+                lastKey = "d";
+                player.isFacingLeft = false;
+                props.closeInventory();
+                break;
+            case "e":
+                if (playerIsAtInteractableBoundary) {
+                    keys.e.pressed = true;
+                    props.onInteraction(interactedBoundary);
+                }
+                break;
+            case "i":
+                if (playerIsAtInteractableBoundary) {
+                    props.showInventory(interactedBoundary);
+                } else {
+                    props.showInventory();
+                }
+                break;
+        }
+    })
+
+    addEventListener("keyup", ({ key }) => {
+        switch (key) {
+            case "w":
+                keys.w.pressed = false;
+                break;
+            case "a":
+                keys.a.pressed = false;
+                break;
+            case "s":
+                keys.s.pressed = false;
+                break;
+            case "d":
+                keys.d.pressed = false;
+                break;
+        }
+    })
+    // }
 
     return (
         <div style={{ width: "fit-content" }}>
